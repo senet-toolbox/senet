4776bfe0651cd6abd3533f92045ac14c25e11abf
vic-Rokx <v.rokx.nellemann@gmail.com>
2025-07-02 20:45:00 -0400

    added fix to the padding

--- src/routes/Page.zig
+++ src/routes/Page.zig
@@ -195,7 +195,7 @@ pub fn render() void {
                 Static.Image("/assets/Logo.svg", .{
                     .width = .percent(70),
                     .display = .Center,
-                    .padding = if (Fabric.isMobile()) .all(0) else .{ .top = 200 },
+                    .padding = if (Fabric.isMobile()) .all(0) else .{ .top = 320 },
                 });
             } else {
                 Static.Image("/assets/Logo.svg", .{
