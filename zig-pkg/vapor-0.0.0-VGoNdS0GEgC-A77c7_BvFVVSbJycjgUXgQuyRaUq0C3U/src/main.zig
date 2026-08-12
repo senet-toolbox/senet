d67aef97999a2113475707eb309418e5ce444e3c
vic-Rokx <v.rokx.nellemann@gmail.com>
2026-08-12 12:37:03 -0400

    removed zig-pkg

--- zig-pkg/vapor-0.0.0-VGoNdS0GEgC-A77c7_BvFVVSbJycjgUXgQuyRaUq0C3U/src/main.zig
+++
@@ -1,7 +0,0 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub const ElementType = @import("root").ElementType;
-// Global buffer to store the CSS string for returning to JavaScript
-pub fn main() !void {}
